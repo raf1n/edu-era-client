de0cf15dbe154efa1cb0029fe2c6b13c62711fd9
raf1n <rafinc10@gmail.com>
2022-10-24 23:40:29 +0600

    folder-structure-and-basic-router-setup-added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./routes/routes";
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [{}],
+  },
+]);
