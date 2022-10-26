549ad34ce3153766da3f6818177dd77aa8f562ff
raf1n <rafinc10@gmail.com>
2022-10-26 14:47:13 +0600

    checkout-page-with-toaster-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./routes/routes";
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
+    </div>
+  );
 }
 
 export default App;
