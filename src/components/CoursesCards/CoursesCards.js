ad43e3e961ef378569b0ffbab074a18e57f9e1d7
raf1n <rafinc10@gmail.com>
2022-10-26 16:14:05 +0600

    course-card-component-updated

--- src/components/CoursesCards/CoursesCards.js
+++ src/components/CoursesCards/CoursesCards.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 const CoursesCards = ({ course }) => {
-  const { name, img, id } = course;
+  const { name, img, id, mentor, enrolled, rating } = course;
   return (
     <Link to={`/course/${id}`}>
       <div className="card card-compact w-80 lg:w-96 bg-base-100 shadow-xl">
@@ -10,7 +10,27 @@ const CoursesCards = ({ course }) => {
           <img className="w-96 h-56" src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{name}</h2>
+          <div className="flex items-center justify-between">
+            <h2 className="card-title">{name}</h2>
+            <h3 className="font-medium text-blue-800 text-base">
+              Mentor: <span className="text-black">{mentor}</span>
+            </h3>
+          </div>
+          <div className="grid grid-cols-2 mx-auto m-2">
+            <p className="text-gray-600 dark:text-black font-semibold">
+              Enrolled: {enrolled}
+            </p>
+            <p className="text-yellow-500 flex items-center">
+              <span className="text-black mr-1 font-semibold">
+                Rating: {rating}
+              </span>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStarHalfAlt></FaStarHalfAlt>
+            </p>
+          </div>
           <div className="card-actions justify-center">
             <button className="btn btn-wide btn-outline">View Details</button>
           </div>
--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import CoursesCards from "../../components/CoursesCards/CoursesCards";
 import CoursesSideBar from "../../components/CoursesSideBar/CoursesSideBar";
@@ -6,12 +6,18 @@ import CoursesSideBar from "../../components/CoursesSideBar/CoursesSideBar";
 const Courses = () => {
   const coursesCategory = useLoaderData();
   //   console.log(coursesCategory);
+  const [allCourse, setAllCourse] = useState([]); //   console.log(coursesCategory);
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setAllCourse(data));
+  }, []);
 
   return (
     <div>
-      <h1 class="text-3xl text-center font-semibold text-gray-800 capitalize lg:text-4xl">
+      <h1 className="text-3xl text-center font-semibold text-gray-800 capitalize lg:text-4xl">
         explore our <br /> awesome{" "}
-        <span class="underline decoration-blue-500">Courses</span>
+        <span className="underline decoration-blue-500">Courses</span>
       </h1>
       <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start lg:justify-evenly mt-6 lg:mt-10">
         <div className="w-52">
@@ -20,7 +26,7 @@ const Courses = () => {
           ))}
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mb-8 mt-4 lg:mt-0">
-          {coursesCategory.map((course) => (
+          {allCourse.map((course) => (
             <CoursesCards key={course.id} course={course}></CoursesCards>
           ))}
         </div>
