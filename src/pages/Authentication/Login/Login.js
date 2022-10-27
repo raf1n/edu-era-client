e449e917f81ab23655974df387bee0d9e449069f
raf1n <rafinc10@gmail.com>
2022-10-27 18:36:41 +0600

    input-field-validation-added

--- src/pages/Authentication/Login/Login.js
+++ src/pages/Authentication/Login/Login.js
@@ -137,6 +137,7 @@ const Login = () => {
                 Email Address
               </label>
               <input
+                required
                 name="email"
                 id="LoggingEmailAddress"
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
@@ -161,6 +162,7 @@ const Login = () => {
               </div>
 
               <input
+                required
                 name="password"
                 id="loggingPassword"
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
